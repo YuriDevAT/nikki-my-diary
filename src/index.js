a73130d33239fe9a345955549ea93b66466d1071
Julia Undeutsch <JUndeutsch@gmx.at>
2021-08-06 14:25:11 +0200

    update index.js with auth0provider component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="REACT_APP_AUTH0_DOMAIN"
+    clientId="REACT_APP_AUTH0_CLIENT_ID"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
