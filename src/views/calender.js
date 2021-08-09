5df21f50b437771e650e570aae7500bc6ef39ceb
Julia Undeutsch <JUndeutsch@gmx.at>
2021-08-09 14:25:59 +0200

    style calender

--- src/views/calender.js
+++ src/views/calender.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import Calender from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const CalenderView = () => {
   const [date, setDate] = useState(new Date());
+  const dateFormat = new Date();
 
   const onChange = (date) => {
     setDate(date);
@@ -13,13 +15,11 @@ const CalenderView = () => {
       <header className="">
         <h1>Calender</h1>
       </header>
-      <div className="sm:w-1/2 h-96 shadow-md p-8 rounded-md">
-        <Calender
-          onChange={onChange}
-          value={date}
-          className=""
-          locale="ja-JA"
-        />
+      <div className="w-full p-8 flex justify-around">
+        <Calender onChange={onChange} value={date} locale="ja-JA" />
+        <div>
+          <h2>{dateFormat.toLocaleString()}</h2>
+        </div>
       </div>
     </div>
   );
