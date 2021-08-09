bea36da0b2007696012e80da48147c25e39456e0
Julia Undeutsch <JUndeutsch@gmx.at>
2021-08-09 17:36:21 +0200

    change input to textarea

--- src/components/entries/AddEntrie.js
+++ src/components/entries/AddEntrie.js
@@ -1,22 +1,29 @@
 import React from "react";
 
-const AddEntrie = ({ entrie, onAddFormSubmit, onAddInputChange }) => {
+const AddEntrie = ({
+  entrie,
+  setEntrie,
+  onAddFormSubmit,
+  onAddInputChange,
+}) => {
   return (
     <form
       onSubmit={onAddFormSubmit}
       className="bg-blue-300 sm:w-1/2 text-center mx-auto"
     >
-      <h2 className="text-2xl py-6">Add Entrie</h2>
+      <h2 className="text-2xl py-6">How was your day?</h2>
       <label htmlFor="entrie">
-        <input
+        <textarea
           name="entrie"
           type="text"
-          placeholder="How was your day?"
           value={entrie}
           onChange={onAddInputChange}
-          className="w-full py-8"
+          className="w-full p-8 border-2 rounded-sm"
         />
       </label>
+      <button type="submit" onClick={() => setEntrie(entrie)}>
+        Add
+      </button>
     </form>
   );
 };
