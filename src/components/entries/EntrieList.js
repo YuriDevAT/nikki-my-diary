9c4d351a436b65f3b4f642591fad678da5dcc271
Julia Undeutsch <JUndeutsch@gmx.at>
2021-08-13 12:23:02 +0200

    create entrielist component for api

---
+++ src/components/entries/EntrieList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Entrie from "./Entrie";
+import Skeleton from "react-loading-skeleton";
+
+const EntrieList = ({ entries, handleEditClick, handleDeleteClick }) => {
+  return (
+    <ul className="bg-green-300 grid grid-cols-3 gap-8 mt-4 mb-12">
+      {entries.map(
+        (entrie) =>
+          (
+            <Entrie
+              key={entrie.id}
+              entrie={entrie}
+              onEditClick={handleEditClick}
+              onDeleteClick={handleDeleteClick}
+            />
+          ) || <Skeleton />
+      )}
+    </ul>
+  );
+};
+
+export default EntrieList;
