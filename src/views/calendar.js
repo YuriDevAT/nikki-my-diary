e6947b16cfdf2e954b951f6aa9a9c73943ec155a
Julia Undeutsch <JUndeutsch@gmx.at>
2021-08-13 15:43:20 +0200

    fetch entries from localStorage

--- src/views/calendar.js
+++ src/views/calendar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Calendar from "react-calendar";
 import Hero from "../components/Hero";
 import "react-calendar/dist/Calendar.css";
@@ -6,54 +6,42 @@ import Time from "../hooks/time";
 
 const CalendarView = () => {
   const [date, setDate] = useState(new Date());
-  const dateFormat = new Date();
-  const [entries, setEntries] = useState([]);
-  const [year, setYear] = useState([dateFormat.getFullYear()]);
-  const [month, setMonth] = useState([dateFormat.getMonth() + 1]);
-  const [day, setDay] = useState([dateFormat.getDate()]);
+  const [entries, setEntries] = useState(() => {
+    const savedEntries = localStorage.getItem("entries");
+    if (savedEntries) {
+      return JSON.parse(savedEntries);
+    } else {
+      return [];
+    }
+  });
 
   const onChange = (date) => {
     setDate(date);
   };
 
-  useEffect(() => {
-    const getEntries = async () => {
-      const res = await fetch(`allEntries${year}-${month}-${day}`);
-      const entrie = await res.json();
-      setEntries(entrie.entrie);
-    };
-    getEntries();
-  }, [day, month, year]);
-
-  let entrieList = entries.map((entrie) => <li key={entrie.id}>{entries}</li>);
-
-  const onClickDay = (date) => {
-    setMonth(date.getMonth() + 1);
-    setDay(date.getDate());
-    setYear(date.getFullYear());
-    const getEntries = async () => {
-      const res = await fetch(``);
-      const entrie = await res.json();
-      setEntries(entrie.entrie);
-    };
-    getEntries();
-  };
+  // let entrieList = entries.map((entrie) => <li key={entrie.id}>{entries}</li>);
 
   return (
     <div className="">
       <Hero heading="What's up for today" />
       <div className="w-full p-8 flex justify-around">
-        <Calendar
-          onChange={onChange}
-          value={date}
-          locale="ja-JA"
-          onClickDay={onClickDay}
-        />
-        <div>
+        <Calendar onChange={onChange} value={date} locale="ja-JA" />
+        <div className="w-1/2 text-center">
           <h2 className="text-2xl">
             {date.toLocaleString().slice(0, 10)}, <Time />
           </h2>
-          <div>{entrieList}</div>
+          <h3 className="py-8">Here are the entries for today</h3>
+          {/* <div>{entrieList}</div> */}
+
+          <ul>
+            {entries.map((entrie) => (
+              <li>{entrie.text}</li>
+            ))}
+          </ul>
+          {!entries ||
+            (entries.length === 0 && (
+              <p>No entries available. Please add some entries.</p>
+            ))}
         </div>
       </div>
     </div>
