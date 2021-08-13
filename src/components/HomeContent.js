f3711ce062a95e42abab006d4cc765c1b1ca28b4
Julia Undeutsch <JUndeutsch@gmx.at>
2021-08-13 11:38:08 +0200

    fetch quote to home

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const HomeContent = () => (
-  <div className="">
-    <h2 className="">What to do? HomeContent Component</h2>
-    <div>
-      <div>
-        <h6 className="">HELLO</h6>
-        <p>hello</p>
-      </div>
+const HomeContent = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  useEffect(() => {
+    getQuote();
+    const intervalID = setInterval(() => {
+      getQuote();
+    }, 24 * 60 * 60 * 1000);
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
+  const getQuote = () => {
+    fetch("http://quotes.rest/qod.json?category=inspire")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data.contents.quotes[0].quote);
+        setAuthor(data.contents.quotes[0].author);
+      });
+  };
+
+  return (
+    <div className="">
+      <h2>{quote}</h2>
+      <p>- {author}</p>
     </div>
-  </div>
-);
+  );
+};
 
 export default HomeContent;
