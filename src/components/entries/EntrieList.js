6cea6c4eb6732017b1408d46a9e2e3a530e68e7d
Julia Undeutsch <JUndeutsch@gmx.at>
2021-08-14 18:02:26 +0200

    add entries count to profile

--- src/components/entries/EntrieList.js
+++ src/components/entries/EntrieList.js
@@ -3,20 +3,28 @@ import Entrie from "./Entrie";
 import Skeleton from "react-loading-skeleton";
 
 const EntrieList = ({ entries, handleEditClick, handleDeleteClick }) => {
+  const entrieCount = entries.length;
+
   return (
-    <ul className="grid grid-cols-3 gap-8 mt-4 mb-12">
-      {entries.map(
-        (entrie) =>
-          (
-            <Entrie
-              key={entrie.id}
-              entrie={entrie}
-              onEditClick={handleEditClick}
-              onDeleteClick={handleDeleteClick}
-            />
-          ) || <Skeleton />
-      )}
-    </ul>
+    <>
+      <h2>
+        You have {entrieCount} {entrieCount === 1 ? `entrie` : `entries`} so
+        far.
+      </h2>
+      <ul className="grid grid-cols-3 gap-8 mt-4 mb-12">
+        {entries.map(
+          (entrie) =>
+            (
+              <Entrie
+                key={entrie.id}
+                entrie={entrie}
+                onEditClick={handleEditClick}
+                onDeleteClick={handleDeleteClick}
+              />
+            ) || <Skeleton />
+        )}
+      </ul>
+    </>
   );
 };
 
