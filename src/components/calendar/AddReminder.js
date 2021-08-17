17df78e9ad2696da87bcdc16014886d054bfd038
Julia Undeutsch <JUndeutsch@gmx.at>
2021-08-17 14:22:05 +0200

    fix bug onSubmit

--- src/components/calendar/AddReminder.js
+++ src/components/calendar/AddReminder.js
@@ -9,19 +9,18 @@ const AddReminder = ({
 }) => {
   return (
     <form
-      onChange={handleReminderSubmitForm}
+      onSubmit={handleReminderSubmitForm}
       className="border-2 flex flex-col  bg-white"
     >
       <h2>What do you want to be reminded of?</h2>
       <label
-        htmlFor="date"
+        htmlFor="dateReminder"
         className="p-2 pl-2 rounded-md shadow-sm outline-none focus:border-gray-600 border-2 m-4"
       >
         Date:
         <input
           type="date"
-          id="date"
-          name="date"
+          name="dateReminder"
           value={date.toLocaleString()}
           required
           onChange={handleDateChange}
