5c6b22210ab4bc42b2acbdd545e359740d9c11ab
Julia Undeutsch <JUndeutsch@gmx.at>
2021-08-18 12:46:29 +0200

    add loading and error to fetch

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 const HomeContent = () => {
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     getQuote();
@@ -14,19 +16,36 @@ const HomeContent = () => {
     };
   }, []);
 
-  const getQuote = () => {
-    fetch("http://quotes.rest/qod.json?category=inspire")
-      .then((res) => res.json())
-      .then((data) => {
-        setQuote(data.contents.quotes[0].quote);
-        setAuthor(data.contents.quotes[0].author);
-      });
+  const getQuote = async () => {
+    setIsLoading(true);
+    setError(false);
+
+    try {
+      const res = await fetch("http://quotes.rest/qod.json?category=inspire");
+      const json = await res.json();
+      setQuote(json.contents.quotes[0].quote);
+      setAuthor(json.contents.quotes[0].author);
+    } catch (error) {
+      setError(true);
+    }
+    setIsLoading(false);
   };
 
   return (
-    <div className="">
-      <h2>{quote}</h2>
-      <p>- {author}</p>
+    <div className="bg-gray-400 h-36 flex justify-center items-center">
+      {error && (
+        <p>
+          I would be a motivating quote but unfortunately something went wrong.
+        </p>
+      )}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <h2>{quote}</h2>
+          <p className="float-right">- {author}</p>
+        </div>
+      )}
     </div>
   );
 };
