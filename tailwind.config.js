1ee7ca3d0d209b7b2eb8da7119f59cf066c7e379
Julia Undeutsch <JUndeutsch@gmx.at>
2021-08-19 19:25:12 +0200

    add color to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,35 @@
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      gray: {
+        base: "#616161",
+        background: "#fafafa",
+        primary: "#dbdbdb",
+      },
+      black: {
+        light: "#262626",
+        faded: "rgba(0,0,0,0.5)",
+      },
+      green: {
+        light: "#D2E4C8",
+        middle: "#7EB363",
+        dark: "#6BA451",
+      },
+      rose: {
+        light: "#F4D8DB",
+        middle: "#E7A8B0",
+        dark: "#DB7582",
+      },
+      brown: {
+        light: "#988281",
+        dark: "#413C2F",
+      },
+    },
     extend: {},
   },
   variants: {
