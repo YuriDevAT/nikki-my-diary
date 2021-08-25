67085f28d2c561cb2b1eb090caa298a22b0e9d1f
Julia Undeutsch <JUndeutsch@gmx.at>
2021-08-25 17:22:07 +0200

    change browserRouter to hashbrowser to fix bug netlify page error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 
 ReactDOM.render(
