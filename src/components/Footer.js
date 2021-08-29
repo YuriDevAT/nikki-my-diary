021bf408a43797d74b823c529662469dd3b14252
Julia Undeutsch <JUndeutsch@gmx.at>
2021-08-29 12:22:31 +0200

    add footer to home component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { LanguageContext } from '../context/Language';
 
-const Footer = () => (
-  <footer className="container text-center pt-24 pb-16 text-lg tracking-wider mx-auto">
-    <p>
-      made with <span className="text-rose-dark">&#10084;</span> and &#9835;
-      2021 by{' '}
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://julia-undeutsch.netlify.app/"
-      >
-        <span>
-          <u>julia undeutsch</u>
-        </span>
-        . all rights reserved.
-      </a>
-    </p>
-  </footer>
-);
+const Footer = () => {
+  const { dictionary } = useContext(LanguageContext);
+
+  return (
+    <footer className="container text-center py-16 text-lg tracking-wider mx-auto">
+      <p>
+        {dictionary.footerMade}
+        <span className="text-rose-dark">&#10084;</span> {dictionary.footerBind}{' '}
+        &#9835; {dictionary.footerPrep} {dictionary.footerDo}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://julia-undeutsch.netlify.app/"
+        >
+          <span>
+            <u>{dictionary.footerName}</u>
+          </span>
+          {dictionary.footerDate}
+          {dictionary.footerRights}
+        </a>
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
